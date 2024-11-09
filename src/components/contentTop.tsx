0562c2af2d4a1bb4ca8c2a5f6c438133118a07d3
CappuccinoZ <zq0410@outlook.com>
2024-11-09 16:48:38 +0800

    update 2024.11.9

--- src/components/contentTop.tsx
+++ src/components/contentTop.tsx
@@ -10,7 +10,7 @@ const ContentTop: React.FC<Props> = ({description}) => {
     return (
         <RounderBox
             height="150px"
-            bgImage="./header.png"
+            bgImage="./header.jpg"
             bgSize="cover"
             bgRepeat="no-repeat"
             display="flex"
